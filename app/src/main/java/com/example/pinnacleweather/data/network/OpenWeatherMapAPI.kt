82f1f9f232a4d6ebc245ac40be993bebe5aa0a13
Amlynar <amlynar93@gmail.com>
2023-10-20 18:08:48 -0400

    Request Imperial Units from the Weather API

--- app/src/main/java/com/example/pinnacleweather/data/network/OpenWeatherMapAPI.kt
+++ app/src/main/java/com/example/pinnacleweather/data/network/OpenWeatherMapAPI.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 interface OpenWeatherMapAPI {
 
     @GET("data/2.5/weather")
-    suspend fun getWeather(@Query("lat") lat: Double, @Query("lon") lon: Double, @Query("appid") apiKey: String): Response<OpenWeatherDataResponse>
+    suspend fun getWeather(@Query("lat") lat: Double, @Query("lon") lon: Double, @Query("units") units: String, @Query("appid") apiKey: String): Response<OpenWeatherDataResponse>
 
 //    @GET("get/1.0/direct?q={cityName},{countryCode}&appid={apiKey}")
 //    suspend fun getLatLon(@Path(value = "cityName") cityName: String, @Path(value = "countryCode") countryCode: String,@Path(value = "cityName") apiKey: String): Response<List<OpenWeatherGeocodingResponse>>
@@ -18,5 +18,5 @@ interface OpenWeatherMapAPI {
     suspend fun getGeoLocation(@Query("q") location: String, @Query("appid") apiKey: String): Response<List<OpenWeatherGeocodingResponse>>
 
     @GET("get/1.0/direct")
-    suspend fun getLatLon(@Query("cityName") cityName: String, @Query("countryCode") countryCode: String,@Query("appid") apiKey: String): Response<List<OpenWeatherGeocodingResponse>>
+    suspend fun getLatLon(@Query("cityName") cityName: String, @Query("countryCode") countryCode: String, @Query("appid") apiKey: String): Response<List<OpenWeatherGeocodingResponse>>
 }
--- app/src/main/java/com/example/pinnacleweather/data/network/WeatherNetworkService.kt
+++ app/src/main/java/com/example/pinnacleweather/data/network/WeatherNetworkService.kt
@@ -7,13 +7,10 @@ class WeatherNetworkService @Inject constructor(
     private val openWeatherMapAPI: OpenWeatherMapAPI) {
 
     private val API_KEY: String = "db62810e9c357e8c3f15f380a8635734"
+    private val UNITS: String = "imperial"
     suspend fun getWeather(lat: Double, lon: Double): Response<OpenWeatherDataResponse> {
-        return openWeatherMapAPI.getWeather(lat,lon,API_KEY)
+        return openWeatherMapAPI.getWeather(lat,lon,UNITS,API_KEY)
     }
-
-//    suspend fun getLatLon(cityName: String): Response<List<OpenWeatherGeocodingResponse>> {
-//        return openWeatherMapAPI.getLatLon(cityName = cityName, countryCode = "US", apiKey = API_KEY)
-//    }
 
     suspend fun getGeoLocation(cityName: String): Response<List<OpenWeatherGeocodingResponse>> {
         return openWeatherMapAPI.getGeoLocation(location = "$cityName,,US", apiKey = API_KEY)
